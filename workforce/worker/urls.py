80b3d424c3ecdd95c5ae2f83e75dc93f2fe82983
Aravinthpromon <aravinth.thillaipandi@promon.co.in>
2024-12-10 19:45:42 +0530

    url change

--- workforce/worker/urls.py
+++ workforce/worker/urls.py
@@ -3,5 +3,5 @@ from .views import WorkerList, WorkerDetail
 
 urlpatterns = [
     path('', WorkerList.as_view(), name='worker-list'),
-    path('<int:pk>/', WorkerDetail.as_view(), name='worker-detail'),
+    path('<int:pk>', WorkerDetail.as_view(), name='worker-detail'),
 ]
